3b1b4b17021dbb82e46aaf4fa5ff1188a7497044
Nanda Blois <nandablois1@gmail.com>
2025-09-29 18:48:42 -0300

    Feat: Inicialização do projeto RBD, definindo fonte e ferramentas do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+import rbd from './assets/rbd.png'
+function App() {
+  
+  return (
+    <>
+    <p className='font-mulish'>rbd teste amdksan</p>
+    <img src={rbd} alt="" />
+    </>
+  )
+}
+
+export default App
